9424b5de704bb805070a4cba07e50439347f2026
Jillian Bartz <97567792+jillianbartz@users.noreply.github.com>
2023-10-24 21:43:18 -0600

    Add files via upload

---
+++ TicTacToe.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int add()
+    {
+    string playerOne;
+    string playerTwo;
+    cout << "Enter name of Player One \n";
+    getline(cin, playerOne);
+    cout << "Enter name of Player Two \n";
+    getline(cin, playerTwo);
+    return 0;
+    }
+
+int main()
+{   
+    int choice;
+    
+    cout  << "menu \n 1. play \n";
+    cin >> choice;
+    cin.ignore(256, '\n');
+    if (choice == 1)
+    {
+        add();
+    }
+
+
+    
+
+    
+
+    cout << "     |     |     \n";
+    cout << "  1  |  2  |  3  \n";
+    cout << "_____|_____|_____\n";
+    cout << "     |     |     \n";
+    cout << "  4  |  5  |  6  \n";
+    cout << "_____|_____|_____\n";
+    cout << "     |     |     \n";
+    cout << "  7  |  8  |  9  \n";
+    cout << "     |     |     \n";
+    
+    return 0;
+}
